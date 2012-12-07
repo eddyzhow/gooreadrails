0a21993a0bdf38734613e4bc762a8cb367f52c14
eddyzhow <grit.ni@gmail.com>
2012-12-07 14:36:07 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gooreadrails::Application.config.secret_token = '1a4ce49bdd9c263442adb073b59b627864f68e5200f6fecaf6e1f17710bdf7574d25cdaa14ef8294e9fceb428dee5a0cb757ae5691472421895ec7106b4886f9'
