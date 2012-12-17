c450098ebaf68fbdb820fcb290ed8cb5ab32aa03
eddyzhow <grit.ni@gmail.com>
2012-12-17 18:53:31 +0700

    session two

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def index
+  	render './gooread'
+  end
+
+  def book_detail
+  	render './bookdetail' + params[:book_id]
+  end
+
 end
